f21cd423359fd540e4da124904b2f012d91ee2eb
Daniel McCloy <dan@mccloy.info>
2024-10-10 12:40:27 -0500

    oops, forgot urljoin is weird

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,5 @@
 from intersphinx_registry import _get_all_mappings, get_intersphinx_mapping
-from urllib.parse import urljoin
+from posixpath import join
 import pytest
 import requests
 
@@ -20,9 +20,7 @@ def test_format(key: str):
     assert "readthedocs.org" not in url, "should be readthedocs.io not org"
 
     if obj is None:
-        requests.head(
-            urljoin(url, "objects.inv"), allow_redirects=True
-        ).raise_for_status()
+        requests.head(join(url, "objects.inv"), allow_redirects=True).raise_for_status()
     else:
         assert obj.startswith("https://"), obj
         assert "readthedocs.org" not in obj
@@ -37,7 +35,7 @@ def test_reach_objects_inv(key: str):
     for v in MAPPING[key]:
         if v is None:
             continue
-        res = requests.head(urljoin(v, "objects.inv"), timeout=TIMEOUT)
+        res = requests.head(join(v, "objects.inv"), timeout=TIMEOUT)
         res.raise_for_status()
         assert res.status_code in (200, 302, CLICK_WRONG_301)
 
