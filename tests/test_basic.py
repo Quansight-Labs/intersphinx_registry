f0340271c083af06dc829e47545be9fc17c0d504
Daniel McCloy <dan@mccloy.info>
2024-10-10 12:27:07 -0500

    relax trailing slash constraint in test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,5 @@
 from intersphinx_registry import _get_all_mappings, get_intersphinx_mapping
+from urllib.parse import urljoin
 import pytest
 import requests
 
@@ -15,14 +16,14 @@ def test_format(key: str):
     assert isinstance(key, str)
     url, obj = MAPPING[key]
     assert url.startswith("https://"), url
-    assert url.endswith("/"), url
     assert "readthedocs.org" not in url, "should be readthedocs.io not org"
 
     if obj is None:
-        requests.head(url + "objects.inv", allow_redirects=True).raise_for_status()
+        requests.head(
+            urljoin(url, "objects.inv"), allow_redirects=True
+        ).raise_for_status()
     else:
         assert obj.startswith("https://"), obj
-        assert obj.endswith("/"), obj
         assert "readthedocs.org" not in obj
         assert obj.startswith(url)
         res = requests.head(obj, allow_redirects=True, timeout=TIMEOUT)
@@ -35,7 +36,7 @@ def test_reach_objects_inv(key: str):
     for v in MAPPING[key]:
         if v is None:
             continue
-        res = requests.head(v + "objects.inv", timeout=TIMEOUT)
+        res = requests.head(urljoin(v, "objects.inv"), timeout=TIMEOUT)
         res.raise_for_status()
         assert res.status_code in (200, 302, CLICK_WRONG_301)
 
