e992f8a479e0216f87bb419a4b802a660a1cf932
M Bussonnier <bussonniermatthias@gmail.com>
2024-11-12 09:27:23 +0100

    release 2024-nov-13

--- intersphinx_registry/__init__.py
+++ intersphinx_registry/__init__.py
@@ -11,7 +11,7 @@ from typing import Dict, Tuple, Set, Optional, cast
 # See issue 4, we this the best format is Major.YYMM.day,
 # in case of multiple releases a day we can borrow the next day's date.
 # no 0 in front of the day as it is not pep440 compliant.
-__version__ = "0.2411.12"
+__version__ = "0.2411.13"
 
 registry_file = Path(__file__).parent / "registry.json"
 
