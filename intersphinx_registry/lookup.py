6226fb007c11e8821629e0248d953d0a7b6aa35d
M Bussonnier <bussonniermatthias@gmail.com>
2024-12-07 12:01:01 +0100

    misc updates and refactor

--- intersphinx_registry/lookup.py
+++ intersphinx_registry/lookup.py
@@ -1,6 +1,7 @@
 import sys
 
 from . import get_intersphinx_mapping
+from . import __version__
 from urllib.parse import urljoin
 
 from typing import Optional
@@ -22,53 +23,61 @@ if len(sys.argv) not in [2, 3]:
         """
     )
 
-packages = set(sys.argv[1].split(","))
 
-search_term: Optional[str]
-if len(sys.argv) == 3:
-    search_term = sys.argv[2]
-else:
-    search_term = None
+def main():
+    print(f"Instersphinx-registry version {__version__}")
 
-# there will be only one url
-urls = [
-    (u[0], (u[1] if u[1] else "objects.inv"))
-    for u in get_intersphinx_mapping(packages=packages).values()
-]
+    packages = set(sys.argv[1].split(","))
 
-flattened = []
-for base_url, obj in urls:
+    search_term: Optional[str]
+    if len(sys.argv) == 3:
+        search_term = sys.argv[2]
+    else:
+        search_term = None
 
-    final_url = urljoin(base_url, obj)
+    # there will be only one url
+    urls = [
+        (u[0], (u[1] if u[1] else "objects.inv"))
+        for u in get_intersphinx_mapping(packages=packages).values()
+    ]
 
-    resp = requests.get(final_url)
+    flattened = []
+    for base_url, obj in urls:
 
-    inv = InventoryFile.load(BytesIO(resp.content), base_url, urljoin)
+        final_url = urljoin(base_url, obj)
 
-    for key, v in inv.items():
-        inv_entries = sorted(v.items())
-        for entry, (_proj, _ver, url_path, display_name) in inv_entries:
-            # display_name = display_name * (display_name != '-')
-            flattened.append((key, entry, _proj, _ver, display_name, url_path))
+        resp = requests.get(final_url)
 
-filtered = []
+        inv = InventoryFile.load(BytesIO(resp.content), base_url, urljoin)
 
-width = [len(x) for x in flattened[0]]
+        for key, v in inv.items():
+            inv_entries = sorted(v.items())
+            for entry, (_proj, _ver, url_path, display_name) in inv_entries:
+                # display_name = display_name * (display_name != '-')
+                flattened.append((key, entry, _proj, _ver, display_name, url_path))
 
-for item in flattened:
-    key, entry, proj, version, display_name, url_path = item
-    if (
-        (search_term is None)
-        or (search_term in entry)
-        or (search_term in display_name)
-        or (search_term in url_path)
-    ):
-        filtered.append((key, entry, proj, version, display_name, url_path))
-        width = [max(w, len(x)) for w, x in zip(width, item)]
+    filtered = []
+
+    width = [len(x) for x in flattened[0]]
+
+    for item in flattened:
+        key, entry, proj, version, display_name, url_path = item
+        if (
+            (search_term is None)
+            or (search_term in entry)
+            or (search_term in display_name)
+            or (search_term in url_path)
+        ):
+            filtered.append((key, entry, proj, version, display_name, url_path))
+            width = [max(w, len(x)) for w, x in zip(width, item)]
 
 
-for key, entry, proj, version, display_name, url_path in filtered:
-    w_key, w_entry, w_proj, w_version, w_di, w_url = width
-    print(
-        f"{key:<{w_key}}  {entry:<{w_entry}}  {proj:<{w_proj}}  {version:<{w_version}}  {display_name!r:<{w_di+2}}  {url_path}"
-    )
+    for key, entry, proj, version, display_name, url_path in filtered:
+        w_key, w_entry, w_proj, w_version, w_di, w_url = width
+        print(
+            f"{key:<{w_key}}  {entry:<{w_entry}}  {proj:<{w_proj}}  {version:<{w_version}}  {display_name!r:<{w_di+2}}  {url_path}"
+        )
+
+
+if __name__ == "__main__":
+    main()
