2e866f53a97faf2a26a0cbd59d9c52a152360258
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-10-21 14:07:59 +0530

    Also allow `-v` as a shorthand for `--version`

--- intersphinx_registry/cli.py
+++ intersphinx_registry/cli.py
@@ -25,7 +25,7 @@ def main():
     )
 
     parser.add_argument(
-        '--version',
+        '-v', '--version',
         action='version',
         version=f'intersphinx-registry {__version__}'
     )
