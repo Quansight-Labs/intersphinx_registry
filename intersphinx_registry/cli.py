08b0de0ce41c084e0f19518f7c8102e697ef5540
M Bussonnier <bussonniermatthias@gmail.com>
2025-11-29 10:42:09 +0100

    Add cache, and ability to do reverse-lookup.
    python -m intersphinx_registry reverse-lookup https://numpy.org/doc/stable/reference/arrays.html will find the rst entry you need to add to your documentation to link to that given url.
    
    ```
    grep -RhoE --include="*.rst" 'https?://[^"'\'' <>]*' . | sort | uniq |xargs python -m intersphinx_registry rev
    ```
    
    Can find urls that could be intersphinxified.

--- intersphinx_registry/cli.py
+++ intersphinx_registry/cli.py
@@ -1,11 +1,16 @@
 import argparse
 import sys
+
 from intersphinx_registry import __version__
+from intersphinx_registry.lookup import (
+    _are_dependencies_available,
+    lookup_packages,
+    reverse_lookup,
+    clear_cache,
+)
 
 
 def lookup_command(args):
-    from intersphinx_registry.lookup import _are_dependencies_available, lookup_packages
-
     if not _are_dependencies_available():
         sys.exit(1)
 
@@ -17,6 +22,21 @@ def lookup_command(args):
         sys.exit(0)
 
     lookup_packages(args.packages, args.search_term)
+
+
+def reverse_lookup_command(args):
+    if not args.urls:
+        print("Usage: intersphinx-registry reverse-lookup <url> [url ...]\n")
+        print("Examples:")
+        print("  intersphinx-registry reverse-lookup https://numpy.org/doc/stable/reference/arrays.html")
+        print("  intersphinx-registry reverse-lookup https://docs.python.org/3/ https://numpy.org/doc/stable/")
+        sys.exit(0)
+
+    reverse_lookup(args.urls)
+
+
+def clear_cache_command(args):
+    clear_cache()
 
 
 def main():
@@ -59,6 +79,39 @@ def main():
         help="Optional search term to filter results",
     )
     lookup_parser.set_defaults(func=lookup_command)
+
+    reverse_lookup_parser = subparsers.add_parser(
+        "reverse-lookup",
+        help="Find which packages documentation URLs belong to",
+        description="Given URLs, find which packages they come from",
+    )
+    reverse_lookup_parser.add_argument(
+        "urls",
+        nargs="*",
+        default=None,
+        help="URLs to look up (space-separated)",
+    )
+    reverse_lookup_parser.set_defaults(func=reverse_lookup_command)
+
+    rev_parser = subparsers.add_parser(
+        "rev",
+        help="Alias for reverse-lookup",
+        description="Given URLs, find which packages they come from",
+    )
+    rev_parser.add_argument(
+        "urls",
+        nargs="*",
+        default=None,
+        help="URLs to look up (space-separated)",
+    )
+    rev_parser.set_defaults(func=reverse_lookup_command)
+
+    clear_cache_parser = subparsers.add_parser(
+        "clear-cache",
+        help="Clear the intersphinx inventory cache",
+        description="Clear the cached intersphinx inventory files",
+    )
+    clear_cache_parser.set_defaults(func=clear_cache_command)
 
     args = parser.parse_args()
 
